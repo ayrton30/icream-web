f07bffafb453bc9da581d8ae88201f49c6a789ce
abetti <ayrton8030@hotmail.com>
2021-11-24 09:44:27 -0300

    added new option to select ice cream flavours

--- js/funciones.js
+++ js/funciones.js
@@ -1,22 +1,39 @@
+// Para manejar el boton que muestra o no los helados disponibles
+let mostrar = true;
+$("#btn_helados").on("click", () => {
+  if (mostrar) {
+    // Tengo que mostrar los helados
+    mostrarHelados(heladosDisponibles);
+    // Cambio la opción del boton
+    $("#btn_helados").html(`<i class="fas fa-eye-slash"></i> Ocultar helados`);
+  } else {
+    $("#contenedorHelados").html("");
+    $("#btn_helados").html(`<i class="fas fa-eye"></i> Mostrar helados`);
+  }
+  mostrar = !mostrar;
+});
+
+//Función para mostrar todos los helados disponibles a la venta.
 function mostrarHelados(helados) {
   for (const helado of helados) {
     $("#contenedorHelados").append(`
     <div class="col"> 
-        <article class="container d-flex flex-column justify-content-around
-          align-items-center tienda__helado mb-3">
-          <img class="rounded-circle mt-1 tienda__helado__img"
-          src="../${helado.img}"
-          alt=""/>
-          <h1 class="fs-1 tienda__helado__title">Helado de ${helado.sabor}</h1>
-          <p class="fs-5 lh-sm">
-            ${helado.descripcion}
-          </p>
-        </article>
+      <article class="container d-flex flex-column justify-content-around
+      align-items-center tienda__helado mb-3">
+        <img class="rounded-circle mt-1 tienda__helado__img"
+        src="../${helado.img}"
+        alt=""/>
+        <h1 class="fs-1 tienda__helado__title">Helado de ${helado.sabor}</h1>
+        <p class="fs-5 lh-sm">
+          ${helado.descripcion}
+        </p>
+      </article>
     </div>
-    `);
+  `);
   }
 }
 
+//Función para mostrar todos los combos de helados disponibles a la venta.
 function mostrarCombos(combos) {
   let contenedorCombos = document.getElementById("contenedorCombos");
 
@@ -32,8 +49,9 @@ function mostrarCombos(combos) {
     let boton = document.createElement("button");
     boton.className = "px-3 fs-3 shadow-lg text-light tienda__button";
     boton.innerHTML = `<i class="fas fa-cart-plus"></i> $${combo.precio}`;
-    boton.id = combo.id;
+    boton.id = combo.tipo;
 
+    //Cada combo tiene su boton al cual se le agrega un evento de click
     boton.addEventListener("click", (event) => {
       //Obtenemos el listado almacenado
       const combos = JSON.parse(localStorage.getItem("combosCarrito"));
@@ -41,7 +59,9 @@ function mostrarCombos(combos) {
 
       console.log("El usuario agregó un producto al carrito");
       console.log(event.currentTarget.id);
-      carrito.agregarCombo(new Combo(parseInt(event.currentTarget.id)));
+      carrito.agregarCombo(
+        new Combo(carrito.carro.length + 1, parseInt(event.currentTarget.id))
+      );
       console.log(carrito);
 
       // Se almacena array completo de combos en carrito
@@ -64,38 +84,73 @@ function mostrarCarrito(carrito) {
   $("#contenedorCarrito").html("");
   $("#contenedorCarrito").addClass("container");
   $("#contenedorCarrito").hide();
-  //let combos = carrito.carro;
 
-  let i = 1;
-  combos.forEach((element) => {
-    $("#contenedorCarrito").append(`<div class="tienda__div my-2 p-1"> 
-    <div class="d-flex flex-column align-items-center">
-    <h1 id="btn_combo_${i}" class="mt-1 px-3 fs-5 shadow-lg text-light tienda__button"><i class="fas fa-times"></i></h1>  
-    <h1 class="fs-2 text-center fw-bold">Combo de ${element.cantidad} kg.</h1>
-    <h1 class="fs-5">Sabores:</h1>
-    <h1 class="mt-1 px-3 fs-4 shadow-lg text-light tienda__button">$${element.precio}</h1>
-    </div>
+  for (let i = 0; i < combos.length; i++) {
+    let element = combos[i];
+    $("#contenedorCarrito").append(`
+    <div class="tienda__div my-2 p-1"> 
+      <div class="d-flex flex-column align-items-center">
+        <h1 id="btn_combo_${i}" class="mt-1 px-3 fs-5 shadow-lg text-light tienda__button">
+        <i class="fas fa-times"></i></h1>  
+        <h1 class="fs-2 text-center fw-bold">Combo de ${element.cantidad} kg.</h1>
+        <div id="saboresHelados-${i}"></div>
+        <h1 class="mt-3 px-3 fs-4 shadow-lg text-light tienda__button">$${element.precio}</h1>
+      </div>
     </div>`);
 
-    $(`#btn_combo_${i}`).on("click", (event) => {
+    //Se dibuja los helados disponibles en cada combo comprado (en el carrito)
+    //element => helado
+    //combo[i] => combo en particular
+    heladosDisponibles.forEach((element) => {
+      $(`#saboresHelados-${i}`)
+        .append(`<img id="${element.id}-${i}" class="rounded-circle mt-1 tienda__sabor"
+                  src="../${element.img}"
+                  alt=""/>`);
+
+      // Si esta el sabor de helado en localStorage
+      if (carrito.carro[i].sabores.find((n) => n.id === element.id)) {
+        $(`#${element.id}-${i}`).addClass("tienda__sabor-selected");
+      }
+
+      //Cada imagen del sabor de helado tiene un evento asociado el cual
+      //indica si ese sabor esta seleccionado o no
+      $(
+        $(`#${element.id}-${i}`).on("click", () => {
+          console.log(`Click sabor: ${element.id}-${i}`);
+
+          //Si no se encuentra seleccionado
+          if (!$(`#${element.id}-${i}`).hasClass("tienda__sabor-selected")) {
+            if (agregarSabor(carrito.carro[i], element)) {
+              $(`#${element.id}-${i}`).addClass("tienda__sabor-selected");
+              guardarLocal("combosCarrito", JSON.stringify(carrito.carro));
+            }
+          } else {
+            quitarSabor(carrito.carro[i], element);
+            $(`#${element.id}-${i}`).removeClass("tienda__sabor-selected");
+            guardarLocal("combosCarrito", JSON.stringify(carrito.carro));
+          }
+        })
+      );
+    });
+
+    $(`#btn_combo_${i}`).on("click", () => {
       carrito.quitarCombo(element);
       // Se almacena array completo de combos en carrito
       guardarLocal("combosCarrito", JSON.stringify(carrito.carro));
       mostrarCarrito(carrito);
     });
+  }
 
-    i++;
-  });
-
+  //Si existe algún combo en el carrito entonces se agregan elementos al DOM
+  //que permiten aplicar descuentos y finalizar la compra
   if (combos.length != 0) {
     $("#contenedorCarrito").append(
-      `<div class="table text-end helados__table">
+      `<div class="table px-4 py-2 rounded text-end helados__table">
         <h1 class="fs-4 mb-2 p-1">Total de la compra: $${carrito.totalCompra()}</h1>
         <div class="d-flex align-items-baseline justify-content-end">
           <h1 class="fs-4 mb-2 p-1">Código:</h1>
-          <input class="w-50" type="text" id="descIngresado">
-          <div 
-          onclick="buscarDescuento();" 
+          <input class="w-50 text-center border-2 contacto__input" type="text" id="descIngresado">
+          <div onclick="buscarDescuento();" 
           class="rounded-3 tienda__button text-light">
             <i class="fas fa-plus"></i>
           </div>
@@ -115,18 +170,42 @@ function mostrarCarrito(carrito) {
       <i class="fas fa-money-bill-wave"></i> Finalizar compra</h1>  
       `
     );
-
+    $("#totalConDescuento").text(`$${carrito.totalCompra()}`);
     $("#contenedorCarrito").fadeIn(150);
 
     // Para manejar el boton de Finalizar compra
-    $("#btn_FinalizarCompra").on("click", () => {
-      console.log("Compra finalizada");
-    });
+    finalizarCompra();
   }
+}
+
+function finalizarCompra() {
+  $("#btn_FinalizarCompra").on("click", () => {
+    let finalizado = true;
+    let combosCarrito = carrito.carro;
+
+    combosCarrito.forEach((combo) => {
+      //Si algun combo no tiene seleccionado ningun helado
+      if (combo.sabores.length == 0) {
+        finalizado = false;
+      }
+    });
+
+    if (finalizado) {
+      console.log("Compra finalizada");
+      console.log(carrito);
+      alert(`Total de la compra: ${$("#totalConDescuento").text()}`);
+    } else {
+      alert(
+        "Por favor seleccione como mínimo un sabor para cada combo en el carro."
+      );
+    }
+  });
 }
 
 function buscarDescuento() {
   const URLGET = "../descuentos.json";
+  //const URLGET =
+  //"https://raw.githubusercontent.com/ayrton30/icream-web/cursoJS/descuentos.json";
 
   $.get(URLGET, function (respuesta, estado) {
     if (estado === "success") {
--- js/tienda.js
+++ js/tienda.js
@@ -1,32 +1,63 @@
+//Se define la clase Helado
 class Helado {
-  constructor(sabor, descripcion, img, color, stock) {
+  constructor(id, sabor, descripcion, img, stock) {
+    this.id = id;
     this.sabor = sabor;
     this.descripcion = descripcion;
     this.img = img;
-    this.stock = stock; //kg
+    this.stock = stock;
   }
 }
 
 class Combo {
-  constructor(id) {
+  constructor(id, tipo) {
     this.id = id;
-    switch (id) {
+    this.tipo = tipo;
+    this.sabores = [];
+
+    switch (tipo) {
       case 1:
         this.precio = 299;
         this.cantidad = "1/4";
+        this.numSabores = 2;
         break;
       case 2:
         this.precio = 549;
         this.cantidad = "1/2";
+        this.numSabores = 3;
         break;
       case 3:
         this.precio = 999;
         this.cantidad = "1";
+        this.numSabores = 4;
         break;
 
       default:
         break;
     }
+  }
+}
+
+function agregarSabor(combo, helado) {
+  // Se agrega un sabor al combo segun la cantidad de sabores del combo
+  //disponibles y si ese sabor no esta repetido
+  if (
+    combo.sabores.length < combo.numSabores &&
+    combo.sabores.find((i) => i.id === helado.id) == undefined
+  ) {
+    combo.sabores.push(helado);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function quitarSabor(combo, helado) {
+  // Solo se llama a esta funcion cuando ya hay un helado seleccionado
+  let indexCombo = combo.sabores.findIndex((i) => i.id === helado.id);
+
+  if (indexCombo > -1) {
+    combo.sabores.splice(indexCombo, 1);
   }
 }
 
@@ -45,6 +76,7 @@ class carroDeCompras {
     if (indexCombo > -1) {
       this.carro.splice(indexCombo, 1);
     }
+    updateIndex(this.carro);
   }
 
   totalCompra() {
@@ -56,21 +88,67 @@ class carroDeCompras {
   }
 }
 
+function updateIndex(carro) {
+  for (let index = 0; index < carro.length; index++) {
+    carro[index].id = index + 1;
+  }
+}
+
 const guardarLocal = (clave, valor) => {
   localStorage.setItem(clave, valor);
 };
 
 //MAIN
 
-// Se crea el carrito de compras
+//Se cargan los helados disponibles, es decir, los sabores que el cliente
+//podra elegir al momento de seleccionar un combo
+let heladoFrutilla = new Helado(
+  1,
+  "frutilla",
+  "Delicioso helado a base de agua, elaborado con fresas, frutillas, agraz y mora frescas de la región. Especial para las personas que adoran la frescura particular de una frutilla.",
+  "img/frutilla.jpg",
+  10
+);
+let heladoVainilla = new Helado(
+  2,
+  "vainilla",
+  "Nuestro helado más popular, elaborado a partir de extractos de vainilla de alta calidad conocida como Madagascar, con un proceso de fabricación exclusivo que le da un aspecto muy cremoso.",
+  "img/vainilla.jpg",
+  10
+);
+let heladoDulceLeche = new Helado(
+  3,
+  "dulce de leche",
+  "Nuestra receta más antigua, este helado de crema a base de dulce de leche Premium Argentino incorporá características únicas que le dan ese sabor tan reconocible por todos los argentinos.",
+  "img/dulcedeleche.jpg",
+  10
+);
+let heladoChocolate = new Helado(
+  4,
+  "chocolate",
+  "Exquisito helado de chocolate elaborado a partir de chocolate vegano orgánico de Perú: la región de cacao más antigua del mundo. Contiene trocitos de chocolate y avellanas.",
+  "img/chocolate.jpg",
+  10
+);
+
+let heladosDisponibles = [
+  heladoFrutilla,
+  heladoVainilla,
+  heladoDulceLeche,
+  heladoChocolate,
+];
+
+//Se crea el carrito de compras
 let carrito = new carroDeCompras();
 // Si aun no se guardo variable en localStorage
 if (localStorage.getItem("combosCarrito") == null) {
   guardarLocal("combosCarrito", JSON.stringify(carrito.carro));
 }
 
-// Se inicializan y guardan los combos disponibles
-const combosDisponibles = [new Combo(1), new Combo(2), new Combo(3)];
+//Se inicializan y guardan los combos disponibles
+//new Combo(id, tipo) donde id es el identificador único y
+//tipo de que combo se trata (250g, 500g ó 1kg)
+const combosDisponibles = [new Combo(1, 1), new Combo(2, 2), new Combo(3, 3)];
 
 // Se almacena array completo
 guardarLocal("combosDisponibles", JSON.stringify(combosDisponibles));
@@ -80,54 +158,8 @@ const combosAlmacenados = JSON.parse(localStorage.getItem("combosDisponibles"));
 const combos = [];
 //Iteramos almacenados con for...of para transformar todos sus objetos a tipo combo.
 for (const objeto of combosAlmacenados) {
-  combos.push(new Combo(objeto.id));
+  combos.push(new Combo(objeto.id, objeto.tipo));
 }
+
 mostrarCombos(combos);
 mostrarCarrito(carrito);
-
-// Se cargan los helados disponibles
-let heladoFrutilla = new Helado(
-  "frutilla",
-  "Delicioso helado a base de agua, elaborado con fresas, frutillas, agraz y mora frescas de la región. Especial para las personas que adoran la frescura particular de una frutilla.",
-  "img/frutilla.jpg",
-  10
-);
-let heladoVainilla = new Helado(
-  "vainilla",
-  "Nuestro helado más popular, elaborado a partir de extractos de vainilla de alta calidad conocida como Madagascar, con un proceso de fabricación exclusivo que le da un aspecto muy cremoso.",
-  "img/vainilla.jpg",
-  10
-);
-let heladoDulceLeche = new Helado(
-  "dulce de leche",
-  "Nuestra receta más antigua, este helado de crema a base de dulce de leche Premium Argentino incorporá características únicas que le dan ese sabor tan reconocible por todos los argentinos.",
-  "img/dulcedeleche.jpg",
-  10
-);
-let heladoChocolate = new Helado(
-  "chocolate",
-  "Exquisito helado de chocolate elaborado a partir de chocolate vegano orgánico de Perú: la región de cacao más antigua del mundo. Contiene trocitos de chocolate y avellanas.",
-  "img/chocolate.jpg",
-  10
-);
-let heladosDisponibles = [
-  heladoFrutilla,
-  heladoVainilla,
-  heladoDulceLeche,
-  heladoChocolate,
-];
-
-// Para manejar el boton que muestra o no los helados disponibles
-let mostrar = true;
-$("#btn_helados").on("click", () => {
-  if (mostrar) {
-    // Tengo que mostrar los helados
-    mostrarHelados(heladosDisponibles);
-    // Cambio la opción del boton
-    $("#btn_helados").html(`<i class="fas fa-eye-slash"></i> Ocultar helados`);
-  } else {
-    $("#contenedorHelados").html("");
-    $("#btn_helados").html(`<i class="fas fa-eye"></i> Mostrar helados`);
-  }
-  mostrar = !mostrar;
-});
